64bab856a224db745ddedc01cb4af588ca32a7a1
Júnior Bocelli <juniorbocelli@gmail.com>
2022-06-08 14:14:53 -0300

    Create post component.

--- src/app/post/create/create.component.ts
+++ src/app/post/create/create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(
+    public postService: PostService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', Validators.required),
+      creator: new FormControl('', Validators.required)
+    });
   }
-
-}
+    
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  get f(){
+    return this.form.controls;
+  }
+    
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  submit(){
+    console.log(this.form.value);
+    this.postService.create(this.form.value).subscribe((res:any) => {
+         console.log('Post created successfully!');
+         this.router.navigateByUrl('post/index');
+    })
+  };
+};
